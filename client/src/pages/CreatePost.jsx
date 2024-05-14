ebd4255c7cfbcb4dfda2327d4fb9151e349416dd
Foti Ceci <foti.ceci@gmail.com>
2024-05-14 16:11:30 -0400

    implement image submission

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -25,7 +25,32 @@ const CreatePost = () => {
     setForm({ ...form, prompt: randomPrompt });
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch("https://localhost:8080/api/v1/post", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert("Success");
+        navigate("/");
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("Please generate an image with proper details");
+    }
+  };
 
   const generateImage = async () => {
     if (form.prompt) {
